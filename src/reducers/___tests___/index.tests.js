5593f8b4eed41378ec4f757fb65bc7f19bfc7380
Aleksey <dovbnya@intaro.ru>
2018-02-21 18:30:57 +0300

    first tests

---
+++ src/reducers/___tests___/index.tests.js
@@ -0,0 +1,17 @@
+import {
+  setPlace,
+  receivedData,
+  receivedError,
+  sortData,
+  removeItem,
+  mapLoaded,
+  getRoute
+} from "../../actions";
+import mainReducer from "../";
+
+describe("Action getRoute", () => {
+  it("Изменяет флаг getRoute", () => {
+    const nextState = mainReducer({ getRoute: false }, getRoute());
+    expect(nextState.getRoute).toEqual(true);
+  });
+});
